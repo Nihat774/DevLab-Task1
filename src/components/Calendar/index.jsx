ffcf89a6fe69102bbde4aab5363cb530ae30dc47
Nihat774 <nihatmemmedov.0520@gmail.com>
2025-05-11 15:10:09 -1200

    added responsive

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -2,8 +2,10 @@ import { DatePicker, DatesProvider } from "@mantine/dates";
 import "@mantine/dates/styles.css";
 import "@mantine/core/styles.css";
 import { useState } from "react";
+import { useMediaQuery } from '@mantine/hooks';
 
-function Demo({ setDate }) {
+function MyCalendar({ setDate }) {
+  const isMobile = useMediaQuery('(max-width: 768px)');
   const [value, setValue] = useState(null);
 
   const handleChange = (e) => {
@@ -13,8 +15,19 @@ function Demo({ setDate }) {
 
   return (
     <DatesProvider settings={{ consistentWeeks: true }}>
-      <DatePicker value={value} onChange={(e)=>handleChange(e)} />
+      <DatePicker
+  value={value}
+  onChange={handleChange}
+  styles={{
+    day: {
+      width: isMobile ? 28 : 36,
+      height: isMobile ? 28 : 36,
+      padding: isMobile ? 3 : 6,
+      fontSize: isMobile ? 12 : 14,
+    },
+  }}
+/>
     </DatesProvider>
   );
 }
-export default Demo;
+export default MyCalendar;
